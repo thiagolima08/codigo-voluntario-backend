9c99d31a274882cb6da3c2200f034d1742232422
carlosnsilva <cmanoel17@gmail.com>
2020-12-14 23:23:33 -0300

    Subindo tentativa de correcao

--- src/main/java/com/projeto/codigovoluntario/CodigoVoluntarioApplication.java
+++ src/main/java/com/projeto/codigovoluntario/CodigoVoluntarioApplication.java
@@ -8,11 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class CodigoVoluntarioApplication {
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
 	public static void main(String[] args) {
 		SpringApplication.run(CodigoVoluntarioApplication.class, args);
 	}
--- src/main/java/com/projeto/codigovoluntario/controller/repositorios/applicationUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projeto.codigovoluntario.controller.repositorios;
-
-import com.projeto.codigovoluntario.model.ApplicationUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface applicationUserRepository extends JpaRepository<applicationUserRepository, Long> {
-    ApplicationUser findByUsername(String username);
-}
