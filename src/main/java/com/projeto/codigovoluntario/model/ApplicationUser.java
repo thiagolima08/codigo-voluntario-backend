8bf552aeec458a49c6aff6cf7fa4d45880290bb4
carlosnsilva <cmanoel17@gmail.com>
2020-12-14 22:32:28 -0300

    Subindo alteracoes JWT

--- src/main/java/com/projeto/codigovoluntario/model/ApplicationUser.java
+++ src/main/java/com/projeto/codigovoluntario/model/ApplicationUser.java
@@ -10,19 +10,19 @@ public class ApplicationUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String email;
+    private String username;
     private String password;
 
     public Long getId(){
         return id;
     }
 
-    public String getEmail() {
-        return email;
+    public String getUsername() {
+        return username;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUsername(String email) {
+        this.username = username;
     }
 
     public String getPassword() {
